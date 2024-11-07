0f4f017a59c343ce811651520fc6cc13ddd9f3cf
bimaadiwijaya <bimaadiwijaya83@gmail.com>
2024-11-07 09:40:09 +0700

    modifikasi pertanyaan nomor 9

--- Bioskop07.java
+++ Bioskop07.java
@@ -19,8 +19,12 @@ public class Bioskop07 {
 
         System.out.println("Penonton pada baris ke-3: ");
 
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
