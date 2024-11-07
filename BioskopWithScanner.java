83734d67b3bda9a2aa0ae6fc9343fd250bf30ad9
bimaadiwijaya <bimaadiwijaya83@gmail.com>
2024-11-07 11:30:46 +0700

    modifikasi

--- BioskopWithScanner.java
+++ BioskopWithScanner.java
@@ -22,18 +22,23 @@ public class BioskopWithScanner {
                     System.out.print("Masukkan nama: ");
                     nama = input.nextLine();
 
-                    System.out.print("Masukkan baris: ");
-                    baris = input.nextInt();
+                    while (true) {
+                        System.out.print("Masukkan baris (1-4): ");
+                        baris = input.nextInt();
 
-                    System.out.print("Masukkan kolom: ");
-                    kolom = input.nextInt();
-                    input.nextLine();
-                    
-                    if (baris > 0 && baris <= 4 && kolom > 0 && kolom <= 2) {
-                        penonton[baris - 1][kolom - 1] = nama;
-                        System.out.println("Data penonton berhasil dimasukkan.");
-                    } else {
-                        System.out.println("Baris atau kolom tidak valid.");
+                        System.out.print("Masukkan kolom (1-2): ");
+                        kolom = input.nextInt();
+                        input.nextLine();
+
+                        if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                            System.out.println("Nomor baris atau kolom tidak valid. Silakan masukkan nomor baris antara 1-4 dan kolom antara 1-2.");
+                        } else if (penonton[baris - 1][kolom - 1] != null) {
+                            System.out.println("Kursi yang dipilih sudah terisi. Silakan pilih kursi lain.");
+                        } else {
+                            penonton[baris - 1][kolom - 1] = nama;
+                            System.out.println("Data penonton berhasil dimasukkan.");
+                            break;
+                        }
                     }
                     break;
 
@@ -41,7 +46,7 @@ public class BioskopWithScanner {
                     System.out.println("\nDaftar Penonton:");
                     for (int i = 0; i < penonton.length; i++) {
                         for (int j = 0; j < penonton[i].length; j++) {
-                            String namaPenonton = (penonton[i][j] != null) ? penonton[i][j] : "-";
+                            String namaPenonton = (penonton[i][j] != null) ? penonton[i][j] : "***";
                             System.out.printf("Baris %d, Kolom %d: %s\n", i + 1, j + 1, namaPenonton);
                         }
                     }
